561ba87e7cc9990951943a5b7a2c1e68920c792b
DannyNepo <147526132+DannyNepo@users.noreply.github.com>
2024-07-06 23:43:20 +0700

    init

---
+++ Task5.5/pt1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace pt1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите текст: ");
+            string[] userText = SplitText(Console.ReadLine());
+            Console.WriteLine("текст состоит из следующих слов: ");
+            ArrayPrint(userText);
+        }
+
+        static string[] SplitText(string text) 
+        {
+            return text.Split(" ");
+        }
+
+        static void ArrayPrint(string[] text) 
+        {
+            for (int i = 0; i < text.Length; i++) 
+            {
+                Console.WriteLine(text[i]);
+            }
+        }
+    }
+}
---
+++ Task5.5/pt2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace pt2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите текст: ");
+            string text = Console.ReadLine();
+            Console.WriteLine("текст наоборот: ");
+            Console.WriteLine(ReverseText(text));
+        }
+
+        static string[] SplitText(string text)
+        {
+            return text.Split(" ");
+        }
+
+        static string ReverseText(string text) 
+        {
+            string[] textArray = SplitText(text);
+            string[] textArrayInversed = new string[textArray.Length];
+            textArrayInversed[0] = textArray[textArray.Length - 1];
+            string resultText = textArrayInversed[0] + " ";
+            for (int i = 1; i < textArray.Length; i++)
+            {
+                textArrayInversed[i] = textArray[textArray.Length - 1 - i];
+                resultText += textArrayInversed[i] + " ";
+            }
+            return resultText;
+        }
+    }
+}
